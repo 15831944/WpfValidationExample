82425ef4ead71b1d0e66901ecd026de784f1f8e6
zhenhua mao <zhenhua.mao@ranplanwireless.com>
2019-05-31 14:33:01 +0800

    Add validation error display style: TextBlock/ ToolTip

---
+++ WpfValidation/ListWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfValidation
+{
+    /// <summary>
+    /// Interaction logic for ListWindow.xaml
+    /// </summary>
+    public partial class ListWindow : Window
+    {
+        public ListWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDisplayErrorByTextBlock_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new TextBlockDisplayErrorWindow();
+            window.Show();
+        }
+
+        private void BtnDisplayErrorByToolTip_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ToolTipDisplayErrorWindow();
+            window.Show();
+        }
+    }
+}
